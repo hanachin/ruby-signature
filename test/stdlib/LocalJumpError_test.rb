eb9e7770e8433b009b78b672db2125626f6c6f4b
Seiei Miyagi <hanachin@gmail.com>
2019-12-18 00:05:11 +0900

    他のreasonの出し方が全く分からないので保留

--- test/stdlib/LocalJumpError_test.rb
+++ test/stdlib/LocalJumpError_test.rb
@@ -13,9 +13,61 @@ class LocalJumpErrorTest < StdlibTest
     end
   end
 
+  def test_reason
+    # :break
+    begin
+      break_in_method.call
+      raise
+    rescue LocalJumpError => exception
+      exception.reason
+    end
+
+    # :redo
+    begin
+      redo_in_method.call
+      raise
+    rescue LocalJumpError => exception
+      exception.reason
+    end
+
+    # :return
+    begin
+      return_from_proc.call
+      raise
+    rescue LocalJumpError => exception
+      exception.reason
+    end
+
+    # :noreason
+    begin
+      no_block
+      raise
+    rescue LocalJumpError => exception
+      exception.reason
+    end
+  end
+
   private
 
   def return_from_proc
     Proc.new { return 42 }
+  end
+
+  def break_in_method
+    capture { break }
+  end
+
+  def redo_in_method
+    while true
+      return capture { redo }
+    end
+  end
+
+  def no_block
+    yield
+  end
+
+  def capture(&b)
+    b
   end
 end
